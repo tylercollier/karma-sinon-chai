0f9c77db8d3017d86b27e24ba4e73d33112ec7b2
Tyler Collier <git@tylercollier.com>
2016-05-16 15:46:56 -0700

    Remove reference to sinon timers and bump version to 1.2.1

--- index.js
+++ index.js
@@ -28,9 +28,7 @@ var framework = function(files) {
   /* Sinon */
   var sinonRoot = path.resolve(require.resolve('sinon'), '../../')
   var sinonPath = path.resolve(sinonRoot, 'pkg/sinon.js');
-  var sinonTimersPath = path.resolve(sinonRoot, 'pkg/sinon-timers.js');
   if (!isDuplicate(sinonPath)) {
-    files.unshift(pattern(sinonTimersPath));
     files.unshift(pattern(lolexPath));
     files.unshift(pattern(sinonPath));
   }
